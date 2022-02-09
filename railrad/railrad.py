47cc866b12016f00713bcaa901cc947488b63248
Jannik Theyssen <25151498+janniktheyssen@users.noreply.github.com>
2022-02-09 12:34:41 +0100

    more specific settings when returning transfer functions

--- railrad/railrad.py
+++ railrad/railrad.py
@@ -46,6 +46,7 @@ class Database():
         self.k = k
         self._check_f_range()
         self._nf = len(f)
+        self._nk = k.shape[1]
 
     def superpose(self, Vn):
         """
@@ -69,6 +70,37 @@ class Database():
                 r = f(f0)  # receiver_nodes, source_nodes
                 self.response[fi, ki, :, :] = np.einsum('is, rs -> ir', Vn[fi, ki], r) * jomega
 
+    def return_tfs(self, f_index=[], k_index=[], receiver_nodes=[], source_nodes=[]):
+        """
+        return the acoustic transfer functions as a 
+
+        tfs has the shape n_f x n_k x n_receiver x n_source
+            where n_f is the number of frequency bins in the spectrum
+                  n_k is the number of wavenumbers at each frequency line,
+                  n_source should be equivalent to the number of source nodes in the database
+                  n_receiver is the number of receiver points
+        """
+        if f_index == []:
+            f_index = np.arange(self._nf)
+        if k_index == []:
+            k_index = np.arange(self._nk)
+        if receiver_nodes == []:
+            receiver_nodes = self.receiver_nodes
+        if source_nodes == []:
+            source_nodes = self.source_nodes
+
+        f = Interp1d_complex(self._f_base, 
+                             self._tf_base[receiver_nodes][:, source_nodes],
+                             assume_sorted=True, copy=False, fill_value=0, bounds_error=False)
+
+        tfs = np.zeros((len(f_index), len(k_index), len(receiver_nodes), len(source_nodes)), dtype=complex)
+        for fi, freq in enumerate(self.f[f_index]):
+            jomega = (1j * 2 * np.pi * freq)
+            for ki, k in enumerate(self.k[fi][k_index]):
+                f0 = f0_shift(k, self.k0, freq)
+                r = f(f0)  # receiver_nodes, source_nodes
+                tfs[fi, ki, :, :] = r * jomega
+        return tfs
 
     def _check_f_range(self):
         if min(self.f) < min(self._f_base):
